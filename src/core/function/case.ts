b5b06952ec96fb0d55e079d1618d38f19a4053b4
Fabien PETITJEAN <tolokoban@Fabiens-MacBook-Pro.local>
2025-01-10 14:45:11 +0100

    Fix camelCase

--- src/core/function/case.ts
+++ src/core/function/case.ts
@@ -1,7 +1,10 @@
+import { TokenCrawler } from "./crawler"
 import { TemplateFunction, TemplateFunctionContext } from "./types"
 import { cap, splitCase } from "./utils"
 
-export const makeFunctionCamel = makeCase("", s => s.toLowerCase())
+export const makeFunctionCamel = makeCase("", (s, i) =>
+    i === 0 ? s.toLowerCase() : cap(s.toLowerCase())
+)
 
 export const makeFunctionPascal = makeCase("", s => cap(s.toLowerCase()))
 
@@ -9,7 +12,7 @@ export const makeFunctionKebab = makeCase("-", s => s.toLowerCase())
 
 export const makeFunctionSnake = makeCase("-", s => s.toLowerCase())
 
-function makeCase(sep: string, map: (text: string) => string) {
+function makeCase(sep: string, map: (text: string, index: number) => string) {
     return (args: TemplateFunction[]): TemplateFunction => {
         return (context: TemplateFunctionContext) => {
             const input = args.map(arg => arg(context)).join("")
